c2d75dbd782ddcd776ff5fffac93c1e5aaa3c8c1
André Peglow <andre.peglow@gmx.net>
2013-08-26 16:56:44 +0200

    made war

--- src/main/java/de/ueberproduct/flypost/HelloWorldServlet.java
+++ src/main/java/de/ueberproduct/flypost/HelloWorldServlet.java
@@ -7,16 +7,15 @@ import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(
-        name = "MyServlet", 
-        urlPatterns = {"/hello"}
-    )
+//@WebServlet(
+//        name = "MyServlet", 
+//        urlPatterns = {"/hello"}
+//    )
 public class HelloWorldServlet extends HttpServlet {
 	
 
